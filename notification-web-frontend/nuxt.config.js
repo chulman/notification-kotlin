7712b9bae1159b6f1286bb5415ca5a888bf94eed
chulman <chlcjfals0122@gmail.com>
2020-11-29 22:08:21 +0900

    build(front-end) : add config

--- notification-web-frontend/nuxt.config.js
+++ notification-web-frontend/nuxt.config.js
@@ -9,21 +9,24 @@ export default {
   head: {
     title: 'notification-web-frontend',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [{
+    src: '~assets/style/base.scss',
+    lang: 'scss'
+  }],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    '~/plugins/http'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -31,10 +34,14 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
+    // https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    //https://http.nuxtjs.org/setup
+    '@nuxt/http'
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
