fc17ea9174d7cdde4f0b78d8d7c9c9afe4c6a3c1
chulman <chlcjfals0122@gmail.com>
2020-12-05 01:21:46 +0900

    feat: message provider 구현 및 테스트

--- notification-api/src/main/kotlin/com/study/notification/api/service/MessageProvider.kt
+++ notification-api/src/main/kotlin/com/study/notification/api/service/MessageProvider.kt
@@ -1,12 +1,13 @@
 package com.study.notification.api.service
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
 import com.google.firebase.messaging.Notification
+import com.study.notification.api.domain.model.Topic
 import com.study.notification.api.domain.repository.NotificationRepository
 import com.study.notification.api.dto.SendResponse
 import com.study.notification.common.utils.LoggerDelegate
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -22,20 +23,20 @@ class MessageProvider(
 
     private val log by LoggerDelegate()
 
-    fun send(topic: String, message: String): SendResponse {
-
-        val message: Message = buildMessage(message, topic)
-
-        log.info("request message = $message")
+    fun send(topic: Topic, message: String): SendResponse {
+        val firebaseMessage: Message = buildMessage(message, topic.topic)
+        log.info("request message = $firebaseMessage")
 
         val messageId = FirebaseMessaging.getInstance()
-            .send(message)
+            .send(firebaseMessage)
 
-        // TODO save repository
-        // notificationRepository.save()
-
-        // TODO response and advisor
-        return SendResponse("", messageId)
+        notificationRepository.save(
+            com.study.notification.api.domain.model.Notification(
+                message,
+                topic
+            )
+        )
+        return SendResponse(HttpStatus.OK, messageId)
     }
 
     private fun buildMessage(
--- notification-api/src/test/kotlin/com/study/notification/api/service/MessageProviderTest.kt
+++ notification-api/src/test/kotlin/com/study/notification/api/service/MessageProviderTest.kt
@@ -1,9 +1,63 @@
 package com.study.notification.api.service
 
-import org.junit.jupiter.api.Assertions.*
+import com.google.firebase.messaging.FirebaseMessaging
+import com.study.notification.api.domain.model.Notification
+import com.study.notification.api.domain.model.Topic
+import com.study.notification.api.domain.repository.NotificationRepository
+import com.study.notification.api.dto.TopicStatus
+import io.mockk.*
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.http.HttpStatus
 
-// TODO - send 메소드 검증 (mocking test)
-// TODO - public 메소드는 모두 검증
+/**
+ * https://mockk.io/
+ */
+@ExtendWith(MockKExtension::class)
 class MessageProviderTest {
 
+    private val notificationRepository
+            = spyk<NotificationRepository>()
+
+    private val firebaseMockk
+            = mockk<FirebaseMessaging>()
+
+    private val messageProvider =
+        MessageProvider(notificationRepository)
+
+    @BeforeEach
+    fun `setUp`() {
+        mockkStatic(FirebaseMessaging::class)
+        every {
+            FirebaseMessaging.getInstance()
+        } returns firebaseMockk
+    }
+
+    @Test
+    fun `send test`() {
+        //given
+        val topic = Topic("news", TopicStatus.ACTIVE)
+        val message = "test message"
+        val notification = Notification(message, topic)
+
+        val messageId = "asd-1111-1234"
+
+        //when
+        every {
+            notificationRepository.save(notification)
+        } returns notification
+
+        every {
+            firebaseMockk.send(any())
+        } returns messageId
+
+        val sendResponse = messageProvider.send(topic, message)
+
+        //then
+        assertEquals(HttpStatus.OK, sendResponse.result)
+        assertEquals(messageId, sendResponse.messageId)
+    }
 }
